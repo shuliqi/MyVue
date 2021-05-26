b8eb5af7fd4cd61bc6d7d46baae2be7311a82b4f
shuliqi <shuliqi@360.cn>
2021-05-26 18:06:18 +0800

    vue 数组的检测

--- js/observer.js
+++ js/observer.js
@@ -1,16 +1,71 @@
+
+// 老的数组的原型
+const oldArrayProperty = Array.prototype;
+
+// 创建一个新的空对象，但是会继承原数组的一些方法，因为当使用我们没有重写的方法的时候，能使用到
+const arrayMethodObj = Object.create(oldArrayProperty);
+
+// 要重写的数组
+const arrayMethods =  ["push", "shift", "unshift", "pop","reverse", "sort", "splice"];
+arrayMethods.forEach(method => {
+  arrayMethodObj[method] = function(...arg) {
+     // 执行老的数组的方法，得到结果
+    const result = oldArrayProperty[method].apply(this, arg);
+    // 进行监听： 1.找到增加的元素； 2.实现监听
+    // 1.找到新增加的元素
+    let inserted;
+    switch(method) {
+      case "push":
+      case "unshift":
+        inserted = arg;
+        break
+      case "splice":
+         // vm.list.splice(3, 0,"哈哈哈", "怎么着") ---> arg = [3, 0, "哈哈哈", "怎么着"]
+        inserted = arg.slice(2);
+        break
+      default:
+        break;
+    }
+    if (inserted) {
+      // 2.实现对新增加元素的监听：当然是新增的元素是数组的话才会去监听，observer函数有处理
+      observerArray(inserted);
+    }
+    console.log(`数组有变化了，方法：${method}, 新增加的值为: ${inserted}`);
+    // 剩下的就需要通知订阅者去更新视图了
+    // ...
+    return result;
+  }
+})
+
+
+
 function Observer(data) {
+  if (Array.isArray(data)) {
+    data.__proto__ = arrayMethodObj;
+    observerArray(data);
+  } else {
+    Object.keys(data).forEach((key) => {
+      defineObserver(data, key, data[key]);
+    })
+  }
+}
+function observerArray(items) {
+  for (let i = 0; i < items.length; i++) {
+    observer(items[i]);
+  }
+}
+
+function observer(value) {
   // 如果数据不存在，或者data 不是一个对象的话， 则不处理
-  if (!data || typeof data !== "object") {
+  if (!value || typeof value !== "object") {
     return;
   }
-  Object.keys(data).forEach((key) => {
-    defineObserver(data, key, data[key]);
-  })
+  return new Observer(value);
 }
 
 function defineObserver(data, key, value) {
   // 监听子元素
-  Observer(data[key]);
+  observer(value);
   const dep = new Dep();
   Object.defineProperty(data, key, {
     get: function() {
@@ -22,7 +77,7 @@ function defineObserver(data, key, value) {
     },
     set: function(newValue) {
       if (value !== newValue) {
-        console.log("监听到变化了", newValue)
+        console.log("监听对象属性到变化了，新的值为：", newValue)
         value = newValue;
         // 通知收集的容器的 notify，notify 去更新每一个订阅者的 update 方法去更新视图
         dep.notify();
